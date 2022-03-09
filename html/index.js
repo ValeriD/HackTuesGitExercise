2a627d730757e3d36e4a8a10e71fb0f6783ea29f
Vasko1234 <vasilvelev07@gmail.com>
2022-03-10 01:16:37 +0200

    moving object with arrow keys

--- html/index.js
+++ html/index.js
@@ -4,6 +4,6 @@ window.addEventListener("keydown", (e) => {
     var objectLeft = parseInt(getComputedStyle(object).getPropertyValue("left"));
     if (e.key == "ArrowLeft" && objectLeft > 0)
         object.style.left = objectLeft - 30 + "px";
-    if (e.key == "ArrowRight" && objectLeft < 400)
+    if (e.key == "ArrowRight" && objectLeft < 900)
         object.style.left = objectLeft + 30 + "px";
 })
